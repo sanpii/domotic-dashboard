1b5098805b1c0a535ac10615a3c3ebf8fcdc82a5
Sanpi <sanpi@homecomputing.fr>
2016-04-30 19:13:53 +0200

    Display weather

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -4,6 +4,7 @@ function IndexController($scope, MQTT_HOST, MQTT_PORT)
 {
     var mqtt = new Paho.MQTT.Client(MQTT_HOST, MQTT_PORT, 'dashboard');
 
+    $scope.weather = {};
     $scope.connected = false;
 
     mqtt.connect({
@@ -11,6 +12,8 @@ function IndexController($scope, MQTT_HOST, MQTT_PORT)
         onSuccess: function () {
             $scope.connected = true;
             $scope.$apply();
+
+            mqtt.subscribe('domotic/weather');
         },
         onFailure: function (message) {
             console.log('Connection failed: ' + message.errorMessage);
@@ -24,6 +27,18 @@ function IndexController($scope, MQTT_HOST, MQTT_PORT)
         if (response.errorCode !== 0) {
             console.log(response.errorMessage);
         }
+    };
+
+    mqtt.onMessageArrived = function (message) {
+        var data = JSON.parse(message.payloadString);
+
+        switch (message.destinationName) {
+            case 'domotic/weather':
+                $scope.weather = data;
+            break;
+        }
+
+        $scope.$apply();
     };
 }
 IndexController.$inject = ['$scope', 'MQTT_HOST', 'MQTT_PORT'];
