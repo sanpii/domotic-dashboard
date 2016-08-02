8238b170064d5f147a251b9447956c5fa4ef9ee3
Sanpi <sanpi@homecomputing.fr>
2016-08-02 20:50:46 +0200

    Highligth on data update

--- web/js/app.js
+++ web/js/app.js
@@ -25,4 +25,24 @@ app.component('graph', {
     }
 });
 
+app.directive('highlighter', ['$timeout', function($timeout) {
+  return {
+    restrict: 'A',
+    scope: {
+      model: '=highlighter'
+    },
+    link: function(scope, element) {
+      scope.$watch('model', function (nv, ov) {
+        if (nv !== ov) {
+          element.addClass('text-success');
+
+          $timeout(function () {
+            element.removeClass('text-success');
+          }, 5000);
+        }
+      });
+    }
+  };
+}]);
+
 app.run();
